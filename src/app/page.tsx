3033262a1baf30e4f5890c253285097ec2988c64
Amon Victor <amon.victor.develops@gmail.com>
2025-06-12 10:30:33 +0300

    Created the Portfolio component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,18 @@
+import About from "@/components/custom/About";
+import Contacts from "@/components/custom/Contact";
+import Header from "@/components/custom/Header";
+import Projects from "@/components/custom/Projects";
 
+export default function Portfolio() {
+  <>
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
+      <Header />
+
+      <main className="container mx-auto px-4 space-y-20">
+        <About />
+        <Projects />
+        <Contacts />
+      </main>
+    </div>
+  </>
+}
